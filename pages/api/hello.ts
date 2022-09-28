5d2c86421f217384ebb8337e3e92a98c1d76aea6
Ellen <ellfiscina@gmail.com>
2022-09-28 13:59:02 +0200

    https

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://api.ravelry.com/',
+  baseURL: 'https://api.ravelry.com/',
   headers: {
     Authorization: 'read-f304127ff38142c8444f373be8dee11e:2Q34GKTk9FEE1MR7Tep7t7tUo4ZNC63eR7b/X2KX'
   }
