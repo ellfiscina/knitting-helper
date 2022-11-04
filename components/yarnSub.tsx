f6d17896424809afc4e6fff86ec77488694a12b1
Ellen <ellfiscina@gmail.com>
2022-11-04 09:24:51 +0100

    search yarn

--- components/yarnSub.tsx
+++ components/yarnSub.tsx
@@ -1,18 +1,61 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+import { Button, Grid, TextField } from "@mui/material";
+import { Yarn } from "../services/types";
 
 const YarnSub = () => {
+  const [name, setName] = useState<string>("");
+  const [yarnList, setYarnList] = useState<Yarn[]>([]);
+
   const getYarn = async () => {
     try {
-      console.log("res");
+      const response = await axios.get("http://localhost:8000/ravelry/yarns", {
+        params: { name },
+      });
+      setYarnList(response.data);
     } catch (error) {
-      return { error };
+      console.log(error);
     }
   };
 
-  useEffect(() => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleOnSubmit = () => {
     getYarn();
-  });
-  return <div>Sub</div>;
+  };
+
+  return (
+    <>
+      <Grid
+        component="form"
+        sx={{ "& > :not(style)": { m: 1 } }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          label="Yarn Name"
+          color="secondary"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          name="query"
+          onChange={handleChange}
+          style={{ width: "310px" }}
+        />
+        <Button
+          className="d-block"
+          variant="contained"
+          color="secondary"
+          type="button"
+          onClick={handleOnSubmit}
+        >
+          Submit
+        </Button>
+      </Grid>
+    </>
+  );
 };
 
 export default YarnSub;
